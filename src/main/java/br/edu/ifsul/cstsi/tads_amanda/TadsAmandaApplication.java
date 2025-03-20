264a4bfc39dd2113204c7d735a2b759d395a2b5f
Amanda Lucena <amandalucena.pl013@academico.ifsul.edu.br>
2025-03-20 20:02:35 -0300

    commit inicial

---
+++ src/main/java/br/edu/ifsul/cstsi/tads_amanda/TadsAmandaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsul.cstsi.tads_amanda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TadsAmandaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TadsAmandaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/edu/ifsul/cstsi/tads_amanda/TadsAmandaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsul.cstsi.tads_amanda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TadsAmandaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
